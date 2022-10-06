21328693ae9e4ff2c89f4179871e3eeff4654712
MarkoJankovic3 <j.marko@teol.net>
2022-10-06 20:14:07 +0200

    Finishing the recipe page

--- src/MealPage.js
+++ src/MealPage.js
@@ -1,23 +1,47 @@
-import { PageHeader, Layout, Typography, Space } from 'antd';
-import { useLocation } from 'react-router-dom';
-import './css/MealPage.css';
+import { PageHeader, Layout, Typography, Image, Space } from "antd";
+import { useLocation } from "react-router-dom";
+import "./css/MealPage.css";
 
 export default function MealPage() {
-    const location = useLocation();
-    const data = location.state.data;
-    const { Header, Content } = Layout;
-    const { Text } = Typography;
-    return (
-        <Layout className='hero'>
-            <PageHeader title={data.title} subTitle={data.dishTypes[0]} onBack={() => window.history.back()} />
-            <Content className='recipe-content'>
-                <Space direction='vertical'>
-                    <Text type='warning'>Cooking time: {data.readyInMinutes} minutes</Text>
-                    <Text type='warning'>Number of serving: {data.servings}</Text>
-                    <Text type='warning'>Price per serving: {data.pricePerServing} $</Text>
-                    <Text type='warning'>For full details please click on the link <a href={data.spoonacularSourceUrl}>here</a></Text>
-                </Space>
-            </Content>   
-        </Layout>
-    );
+  const location = useLocation();
+  const data = location.state.data;
+  const { Header, Content } = Layout;
+  const { Title, Text } = Typography;
+
+  return (
+    <Layout className="meal-hero">
+      <Content className="bg-overlay" />
+      <PageHeader
+        className="return-header"
+        title="Recipes"
+        onBack={() => window.history.back()}
+      />
+      <Content className="recipe-content">
+        <Header className="recipe-header">
+          <Title level={1} type="warning">
+            {data.title}
+          </Title>
+          <Image width={400} src={data.image} />
+        </Header>
+        <Content className="recipe-list">
+          <Space direction="vertical">
+            <Text type="warning">
+              Cooking time: {data.readyInMinutes} minutes
+            </Text>
+
+            <Text type="warning">Number of serving: {data.servings}</Text>
+
+            <Text type="warning">
+              Price per serving: {data.pricePerServing} $
+            </Text>
+
+            <Text type="warning">
+              For full details please click on the link{" "}
+              <a href={data.spoonacularSourceUrl}>here</a>
+            </Text>
+          </Space>
+        </Content>
+      </Content>
+    </Layout>
+  );
 }
